42175e739a4b4f3925c28c35340cf0ac623f6c3c
gauravshilpakar <gaurav.shilpakar@gmail.com>
2022-06-19 21:00:42 -0500

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
+import { ContactComponent } from './contact/contact.component';
+import { BeyondComponent } from './beyond/beyond.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
+    BeyondComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/beyond/beyond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beyond',
+  templateUrl: './beyond.component.html',
+  styleUrls: ['./beyond.component.css']
+})
+export class BeyondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
