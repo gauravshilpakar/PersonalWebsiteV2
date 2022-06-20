ee9c1c37782df96d50b104f4b74754533406a03c
gauravshilpakar <gaurav.shilpakar@gmail.com>
2022-06-20 13:41:46 -0500

    procfile

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/personalwebsitev2'));
-
+app.use(express.static(__dirname + '/dist/PersonalWebsiteAngular'));
 app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/personalwebsitev2/index.html'));
+    res.sendFile(path.join(__dirname +
+        '/dist/PersonalWebsiteAngular/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
